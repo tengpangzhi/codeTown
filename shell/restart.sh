0a65fb96f600404618a2313570d71833c186ada0
tengpangzhi <2740425104@qq.com>
2018-09-08 14:46:22 +0800

    Tomcat重启脚本

---
+++ shell/restart.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+tomcat_home=/usr/tomcat/apache-tomcat-9.0.11
+SHUTDOWN=$tomcat_home/bin/shutdown.sh
+STARTTOMCAT=$tomcat_home/bin/startup.sh
+echo "关闭$tomcat_home"
+$SHUTDOWN
+ps -ef |grep tomcat |grep $tomcat_home |grep -v 'grep'|awk '{print $2}' | xargs kill -9
+
+#删除日志文件，如果你不先删除可以不要下面一行
+rm  $tomcat_home/logs/* -rf
+#删除tomcat的临时目录
+rm  $tomcat_home/work/* -rf
+
+sleep 5
+echo "启动$tomcat_home"
+$STARTTOMCAT
+#看启动日志
+#tail -f $tomcat_home/logs/catalina.out
